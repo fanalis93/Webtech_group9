3326c2b4d3c33d21eb9621adf16b6a6f85106e1b
fanalis93 <fayek.lucse44@gmail.com>
2019-04-28 20:09:49 +0800

    Update script.js

--- Webtech Project/js/script.js
+++ Webtech Project/js/script.js
@@ -8,3 +8,14 @@ function closeNav() {
 }
 /*------------------------------------*/
 
+
+/*header nav*/
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
